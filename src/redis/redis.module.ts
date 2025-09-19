4fb82350325ba5bc96df1d465f68474a39500154
Dieghoatc <dieghoatc@gmail.com>
2025-09-19 10:20:31 -0500

    fix: redis module

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -17,21 +17,10 @@ import Redis from 'ioredis';
     {
       provide: 'REDIS_CLIENT',
       useFactory: () => {
-        const nodeEnv = process.env.NODE_ENV;
+        const host = process.env.REDISHOST || 'localhost';
+        const port = process.env.REDISPORT || 6379;
 
-        if (nodeEnv === 'local') {
-          const host = process.env.REDISHOST || 'localhost';
-          const port = process.env.REDISPORT || 6379;
-
-          return new Redis({ host, port: Number(port) });
-        }
-
-        const redisUrl = process.env.REDIS_URL;
-
-        if (!redisUrl) {
-          throw new Error('Configure REDIS_URL and NODE_ENV=local');
-        }
-        return new Redis(redisUrl);
+        return new Redis({ host, port: Number(port) });
       },
     },
     RedisService,
